051c8aeea9fb2bd6f6919dc347545d38e1e104f0
meaningless-word <grisly@mail.ru>
2022-06-07 10:25:44 +0700

    Аутентификация: Основы

--- Authorization/Startup.cs
+++ Authorization/Startup.cs
@@ -1,24 +1,24 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Authorization
 {
 	public class Startup
 	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddAuthentication("Cookie")
+				.AddCookie("Cookie", config =>
+				{
+					config.LoginPath = "/Admin/Login";
+				});
+			services.AddAuthorization();
+
+			services.AddControllersWithViews();
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
@@ -28,12 +28,12 @@ namespace Authorization
 
 			app.UseRouting();
 
+			app.UseAuthentication();
+			app.UseAuthorization();
+
 			app.UseEndpoints(endpoints =>
 			{
-				endpoints.MapGet("/", async context =>
-				{
-					await context.Response.WriteAsync("Hello World!");
-				});
+				endpoints.MapDefaultControllerRoute();
 			});
 		}
 	}
